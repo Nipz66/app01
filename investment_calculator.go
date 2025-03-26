1583e7aba7e43241eeb416e7bffa0b9bb4cf2c4c
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-03-26 08:22:19 +0530

    function create

--- investment_calculator.go
+++ investment_calculator.go
@@ -12,15 +12,27 @@ func main() {
 	expectetdReturnRate := 5.5
 	var years float64
 
-	fmt.Print("invenstment Amount = ")
+	// fmt.Print("invenstment Amount = ")
+	outputText("invenstment Amount = ")
 	fmt.Scan(&invenstmentAmount)
 
-	fmt.Print("Year = ")
+	// fmt.Print("Year = ")
+	outputText("Year = ")
 	fmt.Scan(&years)
 
 	futureValue := invenstmentAmount * math.Pow(1+expectetdReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println("Future Value : ", futureValue)
-	fmt.Println("Future Real Value : ", futureRealValue)
+	// fmt.Println("Future Value : ", futureValue)
+	// fmt.Println("Future Real Value : ", futureRealValue)
+
+	formatFV := fmt.Sprintf("Future Value : %.1f \n", futureValue)
+	formatRFV := fmt.Sprintf("Future Real Value : %.1f ", futureRealValue)
+
+	fmt.Print(formatFV, formatRFV)
+
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
